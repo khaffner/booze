044ac88c67acfa9526cbb6d775c5c2e1078b7c26
Kevin Haffner <khaffner@gmail.com>
2019-11-13 19:47:13 +0100

    filter out misc products, and better sorting

--- Update-Dump.ps1
+++ Update-Dump.ps1
@@ -1,6 +1,6 @@
 . ./Get-Booze.ps1
 
-$d = Get-Booze
+$d = Get-Booze | Where-Object AlcoholPercentage -GT 0 | Sort-Object Source,ProductNumber
 $d | ConvertTo-Json | Out-File -FilePath "./dump.json" -Force
 $d | Export-Csv -Delimiter ';' -Path "./dump.csv" -Force
 $d | ConvertTo-Html | Out-File -FilePath "./dump.html" -Force
