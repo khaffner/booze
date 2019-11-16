cf4c372dad11bd5b83d859d9a4cf570babb4bfdf
Kevin Haffner <khaffner@gmail.com>
2019-11-16 15:37:45 +0100

    pull first

--- Update-Dump.ps1
+++ Update-Dump.ps1
@@ -1,5 +1,7 @@
 . $PSScriptRoot/Get-Booze.ps1
 
+git pull
+
 $d = Get-Booze | Where-Object AlcoholPercentage -GT 0 | Sort-Object Source,ProductNumber
 $d | ConvertTo-Json | Out-File -FilePath "./dump.json" -Force
 $d | Export-Csv -Delimiter ';' -Path "./dump.csv" -Force
